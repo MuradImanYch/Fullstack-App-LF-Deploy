1a56459fa7d54c40d933f0a45d0b8dfb452528dc
Murad Imanov <mura.imanov@gmail.com>
2023-07-19 00:40:41 +0400

    deploy 5

--- controllers/parser.js
+++ controllers/parser.js
@@ -2959,12 +2959,12 @@ const parsing = async () => {
                 });
             });
     
-            db.query('DELETE FROM unlts', (err => {
+            db.query('DELETE FROM unlTS', (err => {
                 if(err) throw err;
             }));
     
             unlTopScores.map((e) => {
-                db.query('INSERT INTO unlts (place, img, player, games, goals, pen, tLogo, tName) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [e.place, e.img, e.player, e.games, e.goals, e.assists, e.tLogo, e.tName], (err => {
+                db.query('INSERT INTO unlTS (place, img, player, games, goals, pen, tLogo, tName) VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [e.place, e.img, e.player, e.games, e.goals, e.assists, e.tLogo, e.tName], (err => {
                     if(err) throw err;
                 }));
             });
